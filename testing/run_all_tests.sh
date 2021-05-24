6417cd7bf3004fa52ac11ab99acc248639defbfb
Brandi Cantarel <brandi.cantarel@utsouthwestern.edu>
2021-05-24 08:32:12 -0500

    update testing

--- testing/run_all_tests.sh
+++ testing/run_all_tests.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 baseDir="`dirname \"$0\"`"
-
+mkdir ${baseDir}/mouse_se_test
 cd ${baseDir}/mouse_se_test/
+cp ${baseDir}/run_mouse_se_test.sh ${baseDir}/mouse_se_test/run_test.sh
+ln -s /project/shared/bicf_workflow_ref/seqprg/singularity/goalconsortium-*.img .
 sbatch -p 32GB,super run_test.sh
+
+mkdir ${baseDir}/human_pe_test
 cd ${baseDir}/human_pe_test/
+cp ${baseDir}/run_human_pe_test.sh ${baseDir}/human_pe_test/run_test.sh
+ln -s /project/shared/bicf_workflow_ref/seqprg/singularity/goalconsortium-*.img .
 sbatch -p 32GB,super run_test.sh
